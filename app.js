faf9f9b7ad4a74b372d64a94b0d56eb9cddce72a
fianee <2479144850@qq.com>
2025-12-26 15:32:57 +0800

    Update styles

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const botConfigs = {
         theme: 'blue'
     },
     xiaomei: {
-        name: '巩狗媳妇',
+        name: '小美',
         avatar: 'touxiang.jpg',
         appCode: 'H8Cd00WB',
         theme: 'pink'
